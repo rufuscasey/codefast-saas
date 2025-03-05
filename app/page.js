fa835c3451f36e271a619de7521a6c8274a01331
Your Name <you@example.com>
2025-03-04 19:33:04 -0700

    Refactor pricing section layout and enhance content; add features list and update ButtonLogin integration

--- app/page.js
+++ app/page.js
@@ -26,7 +26,7 @@ export default function Home() {
         
       </section>
       {/* HERO */}
-      <section className="text-center px-8  py-32 max-w-3xl mx-auto">
+      <section className="py-32 px-8 max-w-3xl mx-auto text-center">
           <h1 className="text-4xl lg:text-6xl font-extrabold mb-6">
               All your reordering done in 5 minutes
           </h1>
@@ -38,9 +38,64 @@ export default function Home() {
       </section>
       {/* PRICING */}
       <section className='bg-base-200 '>
-        <div className='py-32 px-8 max-w-3xl mx-auto text-center'>
-          <p className='text-sm uppercase font-medium text-primary mb-4'>Pricing</p>
-          <h2>Nothing upfront, pay as you earn</h2>  
+        <div className='py-32 px-8 max-w-3xl mx-auto '>
+          <p className='text-md uppercase font-medium text-primary mb-4 text-center' >Pricing</p>
+          <h2 className='text-3xl lg:text-4xl font-extrabold mb-12 text-center'>Nothing upfront, pay as you go</h2>
+
+          <div className='p-8 bg-base-100 max-w-96 rounded-3xl mx-auto space-y-6'>
+            <div className='flex gap-2 items-baseline '>
+              <div className='text-4xl font-black'>$19</div>
+              <div className=' uppercase text-sm font-medium opacity-60'>/Month</div>
+            </div>  
+            <ul className='space-y-2'>
+              <li className='flex items-center gap-2'>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4">
+                    <path 
+                    fillRule="evenodd" 
+                    d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" 
+                    clipRule="evenodd" 
+                    className='text-green-600 size-4'
+
+                    />
+                </svg>
+                Track all purchases in one place</li>
+              <li className='flex items-center gap-2'>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4">
+                    <path 
+                    fillRule="evenodd" 
+                    d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" 
+                    clipRule="evenodd" 
+                    className='text-green-600 size-4'
+
+                    />
+                </svg>
+                Optimize your product mix</li>
+              <li className='flex items-center gap-2'>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4">
+                    <path 
+                    fillRule="evenodd" 
+                    d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" 
+                    clipRule="evenodd" 
+                    className='text-green-600 size-4'
+
+                    />
+                </svg>
+                Huge timesaver for placing orders</li>
+              <li className='flex items-center gap-2'>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4">
+                    <path 
+                    fillRule="evenodd" 
+                    d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" 
+                    clipRule="evenodd" 
+                    className='text-green-600 size-4'
+
+                    />
+                </svg>
+                Connects to all major POS systems</li>
+            </ul>
+            <ButtonLogin isLoggedIn={isLoggedIn} name={name}/> 
+          </div>
+          
         </div>
       </section>
     </main> 
