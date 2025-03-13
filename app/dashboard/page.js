178e20306ecb14cfa9d49e7532a84e5177412fed
Your Name <rufuscasey1@gmail.com>
2025-03-13 17:46:57 -0600

    refactor Dashboard layout and enhance FormNewBoard component

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,12 +1,23 @@
 import ButtonLogout from "@/components/ButtonLogout";
-import FormNewBoard from "@/components/FormnewBoard";
+import FormNewBoard from "@/components/FormNewBoard";
 
 export default function Dashboard() {
   return (
-    <main>
-      <h1>Dashboard</h1>
-      <ButtonLogout />
-      <FormNewBoard />
+    <main className="bg-base-200 min-h-screen">
+      {/* HEADER */}
+        <section  className="bg-base-100 ">
+          <div className="max-w-5xl mx-auto px-5 py-3 flex justify-end">
+          <ButtonLogout />
+          </div>
+          
+        </section>
+      
+      {/* MAIN */}
+      <section className="max-w-5xl mx-auto px-5 py-12">
+        <FormNewBoard />
+      
+      
+      </section>
       
       
     </main> 
--- components/FormNewBoard.js
+++ components/FormNewBoard.js
@@ -1,5 +1,18 @@
 const FormNewBoard = () => {
-    return <div>FormNewBoard</div>;
+    return (
+    <div className="bg-base-100 p-8 rounded-3xl space-y-8">
+        <label className="form-control w-full ">
+            <div className="label">
+                <span className="label-text">Board Name</span>
+            </div>
+            <input type="text" placeholder="Board Name" className="input input-bordered w-full " 
+            />
+        </label>
+
+       <button className="btn btn-primary w-full">Create Board</button>
+
+    </div>
+    )
 };
 
 export default FormNewBoard;
