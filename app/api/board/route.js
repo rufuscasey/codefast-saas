f94d6c274b6bd83b8e225208cc6d7ede69a8f8fb
Your Name <rufuscasey1@gmail.com>
2025-03-13 17:14:36 -0600

    board creation API

---
+++ app/api/board/route.js
@@ -0,0 +1,51 @@
+import { NextResponse } from "next/server";
+import { auth } from "@/auth";
+import connectMongo from "@/libs/mongoose";
+import User from "@/models/User";
+import Board from "@/models/Board";
+
+
+export async function POST(req) {
+  // do something
+    try {
+        const body = await req.json();
+
+        if (!body.name) {
+            return NextResponse.json(
+                { error: "Board name is required" },
+                { status: 400 }
+            );
+        }
+
+        const session = await auth();
+
+        if (!session) {
+            return NextResponse.json(
+                { error: "Unauthorized" },
+                { status: 401 }
+            );
+        }
+
+        await connectMongo();
+
+        const user = await User.findById(session.user.id);
+
+        const board = await Board.create({
+            userId: user._id,
+            name: body.name,
+        })
+
+        user.boards.push(board._id);
+        await user.save();
+
+        return NextResponse.json({});
+
+        // do something
+    } catch (e) {
+        // send back error
+        return NextResponse.json(
+            { error: e.message },
+            { status: 500 }
+        );
+    }
+}   
