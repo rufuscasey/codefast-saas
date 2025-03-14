e9b3a5cb9fbabbec6932e65bee0dbafb98c9423a
Your Name <rufuscasey1@gmail.com>
2025-03-14 14:08:20 -0600

    create isloading spinner

--- components/FormNewBoard.js
+++ components/FormNewBoard.js
@@ -1,18 +1,53 @@
+"use client";
+
+import { useState } from "react";
+
 const FormNewBoard = () => {
+    const [name, setName] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if (isLoading) return;
+        
+        setIsLoading(true);
+
+        try {
+            // Create a new board via API
+
+        } catch (error) {
+            console.error(error);
+            setIsLoading(false);
+        }
+    }
+    
     return (
-    <form className="bg-base-100 p-8 rounded-3xl space-y-8">
+    <form 
+        className="bg-base-100 p-8 rounded-3xl space-y-8"
+        onSubmit={handleSubmit}
+        >
         <label className="form-control w-full ">
             <div className="label">
                 <span className="label-text">Board Name</span>
             </div>
-            <input type="text" placeholder="Board Name" className="input input-bordered w-full " 
+            <input 
+                type="text" 
+                placeholder="Future Unicorn Corp" 
+                className="input input-bordered w-full "
+                required 
+                value={name}
+                onChange={(event) => setName(event.target.value)}
             />
         </label>
 
-       <button className="btn btn-primary w-full" type="submit">Create Board</button>
+       <button className="btn btn-primary w-full" type="submit">
+        {isLoading && <span className="loading loading-spinner loading-sm"></span>}
+        Create Board</button>
 
     </form>
     )
 };
+
 
 export default FormNewBoard;
