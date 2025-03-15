81b42457fa98e69f5d4d102077fd645f1403986a
Your Name <rufuscasey1@gmail.com>
2025-03-14 18:32:20 -0600

    feat: implement board retrieval and display in FeedbackBoard component

--- app/dashboard/b/[boardId]/page.js
+++ app/dashboard/b/[boardId]/page.js
@@ -1,4 +1,30 @@
+import { redirect } from "next/navigation";
+import connectMongo from "@/libs/mongoose";
+import Board from "@/models/Board";
+import { auth } from "@/auth";
+
+
+const getBoard = async (boardId) => {
+    const session = await auth();
+    
+    await connectMongo();
+    console.log("boardId:", boardId);
+    
+    const board = await Board.findOne({
+        _id: boardId,
+        // filter by userId
+        userId: session?.user?.id
+    });
+    if (!board) {
+        console.log("Board not found");
+        redirect("/dashboard");
+    }
+    
+    return board;
+};
+
 export default async function FeedbackBoard({ params }) {
     const { boardId } = params;
-    return <main>{boardId}</main>;
+    const board = await getBoard(boardId); 
+    return <main>{board.name}</main>;
 }
