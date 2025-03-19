0038c27366ef030214b32bba1ac702a7d344bd62
Your Name <rufuscasey1@gmail.com>
2025-03-19 12:39:06 -0600

    enhance User model with customerId and hasAccess fields and Stripe API success page

--- models/User.js
+++ models/User.js
@@ -1,24 +1,36 @@
+/** @format */
+
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        trim: true
-    },
-    email: {
-        type: String,
-        trim: true,
-        lowercase: true
-    },
-    image: {
-        type: String
-    },
-    boards: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Board"
-        }
-    ]
+	name: {
+		type: String,
+		trim: true,
+	},
+	email: {
+		type: String,
+		trim: true,
+		lowercase: true,
+	},
+	image: {
+		type: String,
+	},
+
+	customerId: {
+		type: String,
+	},
+
+	hasAccess: {
+		type: Boolean,
+		default: false,
+	},
+
+	boards: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Board",
+		},
+	],
 });
 
 export default mongoose.models.User || mongoose.model("User", userSchema);
