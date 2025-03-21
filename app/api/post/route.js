44edaf01409878b7698c75b4e502d718a0a844ac
Your Name <rufuscasey1@gmail.com>
2025-03-21 17:41:16 -0600

    add: implement post deletion feature with access control and ownership checks

--- app/api/post/route.js
+++ app/api/post/route.js
@@ -7,6 +7,7 @@
 import { NextResponse } from "next/server";
 import connectMongo from "@/libs/mongoose";
 import Post from "@/models/Post";
+import User from "@/models/User";
 import { auth } from "@/auth";
 import { Filter } from "bad-words";
 
@@ -44,6 +45,53 @@ export async function POST(req) {
 		return NextResponse.json(post);
 	} catch (e) {
 		console.error(e);
+		return NextResponse.json({ error: e.message }, { status: 500 });
+	}
+}
+
+export async function DELETE(req) {
+	try {
+		const { searchParams } = req.nextUrl;
+		const postId = searchParams.get("postId");
+
+		if (!postId) {
+			return NextResponse.json(
+				{ error: "postId is required" },
+				{ status: 400 }
+			);
+		}
+
+		const session = await auth();
+
+		await connectMongo();
+
+		const user = await User.findById(session.user.id);
+
+		if (!user.hasAccess) {
+			return NextResponse.json(
+				{ error: "Please subscribe first" },
+				{ status: 403 }
+			);
+		}
+		// check the user deleting the post owns the board
+		const post = await Post.findById(postId);
+		if (!post) {
+			return NextResponse.json(
+				{ error: "Post not found" },
+				{ status: 404 }
+			);
+		}
+
+		if (!user.boards.includes(post.boardId.toString())) {
+			return NextResponse.json(
+				{ error: "Posts can only be deleted by the board owner" },
+				{ status: 401 }
+			);
+		}
+
+		await Post.deleteOne({ _id: postId });
+		return NextResponse.json({ message: "Post deleted" });
+	} catch (e) {
 		return NextResponse.json({ error: e.message }, { status: 500 });
 	}
 }
